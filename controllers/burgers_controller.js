42670698bc5a5125d8fbde1d84fe9168b7cfb8a4
CSowers <charliesow@gmail.com>
2018-09-13 22:14:35 -0400

    basically works

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.selectAll(function(data) {
+    burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -25,9 +25,9 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
 
-  console.log("condition", condition);
+    console.log(req.params.id);
 
-  cat.update('devoured', req.body.devoured, req.params.id, function(result) {
+  burger.updateOne('devoured', req.body.devoured, req.params.id, function(result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,6 @@ create database burgers_db;
 Create table burgers(
 	id int not null auto_increment,
     burger_name varchar(30),
-    devoured boolean,
+    devoured boolean default false,
     primary key(id)
 );
