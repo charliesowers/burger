6a717241fedac72e3c9b4c4383d70d8b05403261
CSowers <charliesow@gmail.com>
2018-09-13 21:23:00 -0400

    controller setup

--- config/orm.js
+++ config/orm.js
@@ -7,22 +7,24 @@ var connection = require("./connection.js");
 // These help avoid SQL injection
 // https://en.wikipedia.org/wiki/SQL_injection
 var orm = {
-  selectAll: function(table) {
+  selectAll: function(table, cb) {
     var queryString = "SELECT * FROM ??";
     connection.query(queryString, [table], function(err, result) {
       if (err) throw err;
       console.log(result);
+      cb(result);
     });
   },
-  insertOne: function(table, colName, burger) {
+  insertOne: function(table, colName, burger, cb) {
     var queryString = "INSERT INTO ??(??) values (?)";
     console.log(queryString);
     connection.query(queryString, [table, colName, burger], function(err, result) {
       if (err) throw err;
       console.log(result);
+      cb(result);
     });
   },
-  updateOne: function(table, colName, colVal, id) {
+  updateOne: function(table, colName, colVal, id, cb) {
     var queryString =
       "update ?? set ?? = ? where id = ?";
 
@@ -32,6 +34,7 @@ var orm = {
       function(err, result) {
         if (err) throw err;
         console.log(result);
+        cb(result);
       }
     );
   }
