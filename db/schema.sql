b9c98c20b244ffb4b8babea1c16cd42c1cd14bc7
CSowers <charliesow@gmail.com>
2018-09-13 20:50:10 -0400

    sql setup

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+create database burgers_db;
+ use  burgers_db;
+Create table burgers(
+	id int not null auto_increment,
+    burger_name varchar(30),
+    devoured boolean,
+    primary key(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers(burger_name) values ('The Tunami Burger');
+insert into burgers(burger_name) values ('New Bacon-ings Burger');
+insert into burgers(burger_name) values ('The Fig-eta Bout It Burger');
